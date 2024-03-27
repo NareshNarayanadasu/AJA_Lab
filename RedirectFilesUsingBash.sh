794209c8d8a15d0ec8bf614749dd75e636b872cb
Naresh Narayanadasu <101800735+NareshNarayanadasu@users.noreply.github.com>
2024-03-27 10:12:06 +0530

    Update RedirectFilesUsingBash.sh

--- RedirectFilesUsingBash.sh
+++ RedirectFilesUsingBash.sh
@@ -1,2 +1,3 @@
+#!/bin/bash
 cat > RedirectFilesUsingBashToWrite.sh      # this overrides the script that you are entered in cli
 cat  >>  RedirectFilesUsingBashToWriteAppend.sh    # this script helps you to append the text to the  existing  text
