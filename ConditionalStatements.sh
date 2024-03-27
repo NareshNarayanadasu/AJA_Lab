9f298e67abb9bb37c4d108d09d5b8addba1d4fe9
Naresh Narayanadasu <101800735+NareshNarayanadasu@users.noreply.github.com>
2024-03-27 10:37:05 +0530

    Create ConditionalStatements.sh

---
+++ ConditionalStatements.sh
@@ -0,0 +1,22 @@
+# conditional statements can be used 
+
+
+# example 
+count=10
+if [ $count -eq 10 ]                     # if condition 
+then 
+    echo "the condition is true "
+elif [ $count <= 9 ]                    # elif conditon 
+then 
+    echo "the count is 9 in given condition "
+
+else                                    # else condtion
+    echo " the condition is false "
+if
+
+
+:'  here we can use   iqal to  is   -eq
+                      not iqal to is  -nq
+                      greater then   -gt  or u can use > ny using paranthasis  [ $count -gt 10 ] (( $count > 10 )) 
+                      for less then you can use u can use < ny using paranthasis  [ $count -lt 10 ] (( $count < 10 )) 
+                      and command can be used in   $$  or -a '
